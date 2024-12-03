bf1f0bf5fcba6af2e73c4c78c7abd0784ff981c2
Theideabased <ogunmusireseyi01@gmail.com>
2024-12-03 15:22:53 +0000

    connecting redis sever to node js

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+import redis from 'redis'
+const client = redis.createClient();
+
+client.on("error", function (error) {
+	console.log("Redis client not connected to the server:", error);
+});
+client.on("connect", function (error) {
+	console.log("Redis client connected to the server");
+});
